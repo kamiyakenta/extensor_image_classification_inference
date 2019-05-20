b512af73375ab86f15debfb559e0c8b9c7f26074
kamiyakenta <kentakmy@gmail.com>
2019-05-20 16:37:21 +0900

    commentout

--- lib/consignment_inference.ex
+++ lib/consignment_inference.ex
@@ -1,7 +1,6 @@
 defmodule ConsignmentInrference do
   alias InferenceService, as: IS
 
-
   def consignment_inference do
     model_path = "./pre_trained_model/model.pb"
     label_path = "./labels.txt"
@@ -9,10 +8,11 @@ defmodule ConsignmentInrference do
     service = %IS{graph: model_path, label: label_path, io_info: io_json_path}
     pre_trained_model = IS.load_model(service)
 
+    # -------------------------
     image_path = "./images/image.jpg"
     input_tensor = IS.load_image(pre_trained_model, image_path)
-
     IS.inference(pre_trained_model, input_tensor)
+    # -------------------------
   end
 
 end
