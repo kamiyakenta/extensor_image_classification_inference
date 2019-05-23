b15c1bd14a496c7d8373f29d3c3ea1f1b4c74d65
kamiyakenta <kentakmy@gmail.com>
2019-05-23 15:51:56 +0900

    Exunit

--- test/extensor_inference/my_experiment_code_test.exs
+++ test/extensor_inference/my_experiment_code_test.exs
@@ -1,8 +1,21 @@
 defmodule ExtensorInference.MyExperimentCodeTest do
   use ExUnit.Case, async: true
   alias ExtensorInference.MyExperimentCode, as: MEC
+  alias ImageClassification.InferenceService, as: IS
 
-  test "my_experiment_code" do
+  setup_all do
+    graph = "./pre_trained_model/model.pb"
+    label = "./labels.txt"
+    io_json = "./pre_trained_model/io.json"
+    images = Path.wildcard("./images/**")
+    service = %IS{graph: graph, label: label, io_info: io_json}
+    %{service: service, images: images}
+  end
+
+  test "my_experiment_code", %{service: service, images: images} do
+    assert IS.load_model(service) == :ok
     assert MEC.my_experiment_code() == :ok
   end
 end
+
+# require IEx; IEx.pry()
