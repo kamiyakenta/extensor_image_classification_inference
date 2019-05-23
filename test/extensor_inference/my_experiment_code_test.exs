2d90a0568d750da47911c04dfed7d6bd264400af
kamiyakenta <kentakmy@gmail.com>
2019-05-23 16:09:35 +0900

    separate test

--- test/extensor_inference/my_experiment_code_test.exs
+++ test/extensor_inference/my_experiment_code_test.exs
@@ -7,14 +7,20 @@ defmodule ExtensorInference.MyExperimentCodeTest do
     graph = "./pre_trained_model/model.pb"
     label = "./labels.txt"
     io_json = "./pre_trained_model/io.json"
-    images = Path.wildcard("./images/**")
     service = %IS{graph: graph, label: label, io_info: io_json}
-    %{service: service, images: images}
+    %{service: service}
   end
 
-  test "my_experiment_code", %{service: service, images: images} do
-    assert IS.load_model(service) == :ok
-    assert MEC.my_experiment_code() == :ok
+  describe "my_experiment_code" do
+    test "example result", %{service: service, images: images} do
+      assert IS.load_model(service) == :ok
+      image_path = "./images/galloping_giraffe.png"
+
+    end
+
+    test "Source code itself result" do
+      assert MEC.my_experiment_code() == :ok
+    end
   end
 end
 
