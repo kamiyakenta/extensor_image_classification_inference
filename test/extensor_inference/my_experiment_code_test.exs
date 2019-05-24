643a698c8bd5756c7fe1de2a2c171e407eb9bb5e
kamiyakenta <kentakmy@gmail.com>
2019-05-24 18:11:08 +0900

    edit example result

--- test/extensor_inference/my_experiment_code_test.exs
+++ test/extensor_inference/my_experiment_code_test.exs
@@ -1,6 +1,5 @@
 defmodule ExtensorInference.MyExperimentCodeTest do
   use ExUnit.Case, async: true
-  import ExUnit.CaptureIO
   alias ImageClassification.InferenceService, as: IS
 
   setup_all do
@@ -17,15 +16,10 @@ defmodule ExtensorInference.MyExperimentCodeTest do
       image_path = "./images/galloping_giraffe.png"
       image_path
       |> IS.update_image()
-      output_inference = fn -> IS.inference() end
-      result = String.split(capture_io(output_inference), "   ")
-      assert Enum.at(result, 0) == "084.giraffe"
-      assert Enum.at(result, 1)
-      |> String.replace("\n", "")
-      |> String.to_float() > 0.95
-    end
+      IS.get_inference()
+      [{column, prob}] = List.last(IS.get())
+      assert column == "084.giraffe"
+      assert prob > 0.95
+  end
   end
 end
-
-# require IEx; IEx.pry
-# iex -S mix test --trace
